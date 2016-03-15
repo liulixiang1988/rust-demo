0c621a566660de34df54e8f027cd2734ad7e400d
liulixiang1988 <liulixiang1988@gmail.com>
2016-03-15 22:57:17 +0800

    cast

---
+++ 03-variables-types/cast.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let temp = -3i16;
+    let mut temp2 = 0u16;
+    temp2 = temp as u16;
+    println!("{}", temp2);
+
+    let msg = "12";
+    let msg2 = msg as u32;
+    println!("{}", msg2);
+}
