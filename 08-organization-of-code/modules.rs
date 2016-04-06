c5655d304686015f5c32600a311383acaa504cf4
liulixiang1988 <liulixiang1988@gmail.com>
2016-04-06 10:45:30 +0800

    struct

--- 08-organization-of-code/modules.rs
+++ 08-organization-of-code/modules.rs
@@ -6,9 +6,24 @@ mod game1 {
     pub fn func2(){
         println!("func2调用");
     }
+
+    #[derive(Debug)]
+    pub struct Magician {
+        pub name: String,
+        pub age: i32,
+        power:i32
+    }
+
+    impl Magician {
+        pub fn new(n: String, a: i32, p: i32) -> Magician {
+            Magician { name: n, age: a, power: p}
+        }
+    }
 }
 
 fn main() {
     //game1::func1(); //错误
     game1::func2();
+    let mag1 = game1::Magician::new("Liu Lixiang".to_string(), 28, 100);
+    println!("{:?}", mag1);
 }
