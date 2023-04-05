f8288e869fcae092bb10187317f7cdb7317bc833
Lixiang Liu <liulixiang1988@gmail.com>
2023-04-05 22:11:37 +0800

    data type

---
+++ 2023-book-rust-in-action/ch2/ex01-type/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+    let a = 10;
+    let b: i32 = 20;
+    let c = 30i32;
+    let d = 30_i32;
+    let e = add(add(a, b), add(c, d));
+    println!("{} + {} + {} + {} = {}", a, b, c, d, e);
+
+    let three = 0b11;
+    let thirty = 0o36;
+    let three_hundred = 0x12c;
+    println!("base 10 {} {} {}", three, thirty, three_hundred);
+    println!("base 2 {:b} {:b} {:b}", three, thirty, three_hundred);
+    println!("base 8 {:o} {:o} {:o}", three, thirty, three_hundred);
+    println!("base 16 {:x} {:x} {:x}", three, thirty, three_hundred);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
